5e900a63d0991034804b876457e8f6d2c74e0b89
Andrew Siegman <blerrnk@gmail.com>
2019-02-05 11:51:13 -0600

    Updates

---
+++ precache-manifest.b5dc9276f412a896348abc022552160f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcf3ba2ae8b59b9d9f37",
+    "url": "/camelot/static/js/runtime~main.dcf3ba2a.js"
+  },
+  {
+    "revision": "f079002d3d776824f8f8",
+    "url": "/camelot/static/js/main.f079002d.chunk.js"
+  },
+  {
+    "revision": "e2ef9c5bc3440802d584",
+    "url": "/camelot/static/js/1.e2ef9c5b.chunk.js"
+  },
+  {
+    "revision": "f079002d3d776824f8f8",
+    "url": "/camelot/static/css/main.651d0c8b.chunk.css"
+  },
+  {
+    "revision": "3a19df5b95e17e911b4d28c75912a69b",
+    "url": "/camelot/index.html"
+  }
+];
